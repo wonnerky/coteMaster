d0f4df467e2f94fe742b988d0399676a4ae6ec2e
wonnerky <gandet09@gmail.com>
2022-04-27 21:58:10 +0900

    220427

--- baekjoon/23290.py
+++ baekjoon/23290.py
@@ -12,41 +12,52 @@ def shark_mov(loc, path, num_fish, visited):
         return None
 
     r, c = loc
-    visited[r][c] = True
     for i in range(1, 5):
         nr, nc = r + s_mov[i][0], c + s_mov[i][1]
-        if 0 <= nr < 4 and 0 <= nc < 4 and not visited[nr][nc]:
+        if 0 <= nr < 4 and 0 <= nc < 4:
             n_path = path + str(i)
-            tmp_fish = num_fish + len(board[nr][nc])
-            shark_mov((nr, nc), n_path, tmp_fish, visited)
+            visited_ = copy.deepcopy(visited)
+            if visited_[nr][nc]:
+                tmp_fish = num_fish
+            else:
+                tmp_fish = num_fish + sum(board[nr][nc])
+                visited_[nr][nc] = True
+            shark_mov((nr, nc), n_path, tmp_fish, visited_)
 
 M, S = map(int, input().split())
-board = [[[] for _ in range(4)] for _ in range(4)]
+board = [[[0] * 8 for _ in range(4)] for _ in range(4)]
 for _ in range(M):
     r, c, d = map(int, input().split())
-    board[r - 1][c - 1].append(d - 1)
+    board[r - 1][c - 1][d - 1] += 1
 smell = [[0] * 4 for _ in range(4)]
 r, c = map(int, input().split())
 shark = (r - 1, c - 1)
 smell[r - 1][c - 1] = 1
+
 
 # 0 ~ 7, 좌 좌상 상 상우 우 우하 하 좌하, 시계 방향
 mov =[(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
 # 상 1 좌 2 하 3 우 4
 s_mov = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1)]
 for _ in range(S):
+    print('start')
+    for ele in board:
+        print(ele)
+    print()
     # 복제
     board_ = copy.deepcopy(board)
     # 물고기 이동
     for r in range(4):
         for c in range(4):
-            for d in board_[r][c]:
-                for i in range(8):
-                    dr, dc = mov[(d - i) % 8]
-                    if 0 <= r + dr < 4 and 0 <= c + dc < 4 and smell[r + dr][c + dc] == 0:
-                        board[r][c].remove(d)
-                        board[r+dr][c+dc].append((d - i) % 8)
-                        break
+            for d in range(8):
+                if board_[r][c][d] > 0:  # d 방향에 element 있으면
+                    for i in range(8):
+                        dr, dc = mov[(d - i) % 8]
+                        if 0 <= r + dr < 4 and 0 <= c + dc < 4 and smell[r + dr][c + dc] == 0:
+                            tmp = board_[r][c][d]
+                            board[r+dr][c+dc][(d - i) % 8] += tmp
+                            board[r][c][d] -= tmp
+                            break
     print('smell')
     for ele in smell:
         print(ele)
@@ -65,10 +76,11 @@ for _ in range(S):
     for path in paths:
         r, c = shark[0] + s_mov[int(path)][0], shark[1] + s_mov[int(path)][1]
         shark = (r, c)
-        if board[r][c]:
+        if sum(board[r][c]) > 0:
             smell[r][c] = 3
-            board[r][c] = []
+            board[r][c] = [0] * 8
     print('shark move')
+    print(results)
     for ele in board:
         print(ele)
     print()
@@ -83,7 +95,8 @@ for _ in range(S):
     # 복제
     for r in range(4):
         for c in range(4):
-            board[r][c] += board_[r][c]
+            for i in range(8):
+                board[r][c][i] += board_[r][c][i]
     print('copy')
     for ele in board:
         print(ele)
@@ -94,8 +107,9 @@ for _ in range(S):
         print(ele)
     print()
 
+
 out = 0
 for r in range(4):
     for c in range(4):
-        out += len(board[r][c])
+        out += sum(board[r][c])
 print(out)
